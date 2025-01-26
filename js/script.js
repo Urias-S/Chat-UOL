b57b8ea7ba2829d527b8c16d0ad5cf6069126a94
Vinicius <viniciusmoura761@gmail.com>
2025-01-25 23:41:27 -0300

    feat: select user and visibility to a message

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ let currentUser;
 let username;
 let messages = [];
 let lastMessageCount = 0;
-const loggin = () => {
+let usersOnlineCount = 0;
+const usersContainer = document.querySelector(".contacts");
+function loggin() {
   username = prompt("Qual é o seu nome?");
   const user = {
     name: username
-  }
+  };
   userValidation(user);
 }
 const userValidation = (user) => {
@@ -90,6 +92,140 @@ const renderizeMessages = (requisition) => {
 const errorMessage = () => {
   alert("Ocorreu algum erro, tente novamente mais tarde!");
 }
+
+const searchUser = () => {
+  const promise = axios.get(requisitionsLinkParticipants);
+  promise.then(renderizeUsers);
+}
+const showUsers = () => {
+  const messageData = document.querySelector('.messageData');
+  const background = document.querySelector(".background");
+  background.classList.remove("hidden");
+  messageData.classList.remove("hidden");
+  messageData.classList.add("animate__animated");
+  messageData.classList.add("animate__slideInRight");
+}
+
+const closeUsers = () => {
+  const messageData = document.querySelector('.messageData');
+  const background = document.querySelector(".background");
+  messageData.classList.remove("animate__slideInRight");
+  messageData.classList.add("animate__slideOutRight");
+  setTimeout(() => {
+    background.classList.add("hidden");
+    messageData.classList.add("hidden");
+    messageData.classList.remove("animate__slideOutRight");
+  }, 600);
+}
+const defaultPublic = () => {
+  usersContainer.innerHTML = `
+<button class="contact checkmarked" onclick = "selectUser(this)">
+  <div class="username">
+    <ion-icon name="people"></ion-icon>
+    <h3>Todos</h3>
+  </div>
+  <ion-icon name="checkmark-outline" class="checkmark"></ion-icon>
+</button>`
+}
+const renderizeUsers = (requisition) => {
+  if (usersOnlineCount === requisition.data.length) {
+    return;
+  }
+  let usersOnline = requisition.data;
+  usersOnlineCount = usersOnline.length;
+  for (let i = 0; i < usersOnline.length; i++) {
+    if (usersOnline[i].name !== currentUser) {
+      usersContainer.innerHTML += `
+      <button class="contact" onclick = "selectUser(this)">
+        <div class="username">
+          <ion-icon name="person-circle"></ion-icon>
+          <h3>${usersOnline[i].name}</h3>
+        </div>
+      </button>
+    `
+    }
+  }
+}
+
+const selectUser = (element) => {
+  if (element.classList.contains("checkmarked")) {
+    return;
+  }
+  const alredySelected = document.querySelector(".checkmarked");
+  if (alredySelected) {
+    const checkmarkedToRemove = alredySelected.querySelector('ion-icon[name="checkmark-outline"]');
+    alredySelected.removeChild(checkmarkedToRemove);
+    alredySelected.classList.remove("checkmarked");
+  }
+  element.classList.add("checkmarked");
+  let checkmarkIonIcon = document.createElement('ion-icon');
+  checkmarkIonIcon.setAttribute('name', 'checkmark-outline');
+  checkmarkIonIcon.setAttribute('class', 'checkmark');
+  element.appendChild(checkmarkIonIcon);
+}
+
+const selectPublic = (element) => {
+  if (element.classList.contains("checkmarked")) {
+    return;
+  }
+  const visibility = document.querySelector('.visibility');
+  const alredySelected = visibility.querySelector(".checkmarked");
+  if (alredySelected) {
+    const checkmarkedToRemove = alredySelected.querySelector('ion-icon[name="checkmark-outline"]');
+    alredySelected.removeChild(checkmarkedToRemove);
+    alredySelected.classList.remove("checkmarked");
+  }
+  element.classList.add("checkmarked");
+  let checkmarkIonIcon = document.createElement('ion-icon');
+  checkmarkIonIcon.setAttribute('name', 'checkmark-outline');
+  checkmarkIonIcon.setAttribute('class', 'checkmark');
+  element.appendChild(checkmarkIonIcon);
+}
+
+const renderizeVisibility = () => {
+  const visibility = document.querySelector(".visibility");
+  const buttonPublic = document.createElement("button");
+  const buttonPrivate = document.createElement("button");
+  const div = document.createElement("div");
+  const privateDiv = document.createElement("div");
+  const publicIcon = document.createElement('ion-icon');
+  const privateIcon = document.createElement('ion-icon');
+  const publicTitle = document.createElement('h3');
+  const privateTitle = document.createElement('h3');
+
+  buttonPublic.classList.add("public");
+  visibility.appendChild(buttonPublic);
+  div.classList.add("publicType");
+  buttonPublic.appendChild(div);
+  publicIcon.setAttribute('name', 'lock-open');
+  publicTitle.innerHTML = 'Público';
+  div.appendChild(publicIcon);
+  div.appendChild(publicTitle);
+
+  buttonPrivate.classList.add("private");
+  visibility.appendChild(buttonPrivate);
+  privateDiv.classList.add("publicType");
+  buttonPrivate.appendChild(privateDiv);
+  privateIcon.setAttribute('name', 'lock-closed');
+  privateTitle.innerHTML = 'Reservadamente';
+  privateDiv.appendChild(privateIcon);
+  privateDiv.appendChild(privateTitle);
+
+  buttonPublic.classList.add("checkmarked");
+  let checkmarkIonIcon = document.createElement('ion-icon');
+  checkmarkIonIcon.setAttribute('name', 'checkmark-outline');
+  checkmarkIonIcon.setAttribute('class', 'checkmark');
+  buttonPublic.appendChild(checkmarkIonIcon);
+
+  buttonPublic.setAttribute('onclick', 'selectPublic(this)');
+  buttonPrivate.setAttribute('onclick', 'selectPublic(this)');
+}
+
 loggin();
+getMessages();
+searchUser();
+renderizeVisibility();
+defaultPublic();
 const connectionInterval = setInterval(maintainConnection, 5000);
 const getMessagesInterval = setInterval(getMessages, 3000);
+const usersInterval = setInterval(searchUser, 5000);
